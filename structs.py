ddafc31e8f4acad1fce67a5dd7a2366dce88d541
Bennett Reschke <reschke3@linux-a1.ews.illinois.edu>
2020-10-27 21:41:03 -0500

    Pull PAs from site

---
+++ structs.py
@@ -0,0 +1,20 @@
+class PlateAppearance:
+    
+    def __init__(self, game, inning, play_id, batter, batter_id, swing, pitcher, pitcher_id, pitch, classic_result, result, result_diff, outs, obc):
+        self.game = game
+        self.inning = inning
+        self.play_id = play_id
+        self.batter = batter
+        self.batter_id = batter_id
+        self.swing = swing
+        self.pitcher = pitcher
+        self.pitcher_id = pitcher_id
+        self.pitch = pitch
+        self.classic_result = classic_result
+        self.result = result
+        self.result_diff = result_diff
+        self.outs = outs
+        self.obc = obc
+
+
+    
